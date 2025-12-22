b28e655ea249ad9bfe97dc92f014cb4648fc129d
chenJh-student <13420420247@163.com>
2025-12-22 20:12:19 +0800

    Initial commit: 豆瓣电影网站仿制项目

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://localhost:5000',
+                target: '/DoBian-movie_cjh0120240338/',
                 changeOrigin: true,
                 secure: false
             }
