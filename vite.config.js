7ac8e0bf796e265e6edd9d1659349d6b845698e8
J3-211 <j3211@example.com>
2025-12-23 12:46:39 +0800

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: '/doBian2.0-cjh0120240338/', // GitHub Pages仓库名
     server: {
         port: 3000,
         proxy: {
